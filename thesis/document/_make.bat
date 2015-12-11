a203079e180a17b2fd55e498e129c786f5ed3720
kkthx <lukasz@kkthx.pl>
2015-12-11 02:43:21 +0100

    doc: changed odt to fodt to easier track changes by git

--- thesis/document/_make.bat
+++ thesis/document/_make.bat
@@ -7,12 +7,12 @@ set "lock=%temp%\wait%random%.lock"
 :: Launch processes asynchronously, with stream 9 redirected to a lock file.
 :: The lock file will remain locked until the script ends.
 
-start "" 9>"%lock%1" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 1_wstep.odt tex/1_wstep
-start "" 9>"%lock%2" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 2_analiza_wiedzy.odt tex/2_analiza_wiedzy
-start "" 9>"%lock%3" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 3_teoria_problemu.odt tex/3_teoria_problemu
-start "" 9>"%lock%4" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 4_praktyka_implementacja.odt tex/4_praktyka_implementacja
-start "" 9>"%lock%5" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 5_rezultaty_analiza.odt tex/5_rezultaty_analiza
-start "" 9>"%lock%6" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 6_zakonczenie_podsumowanie.odt tex/6_zakonczenie_podsumowanie
+start "" 9>"%lock%1" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 1_wstep.fodt tex/1_wstep
+start "" 9>"%lock%2" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 2_analiza_wiedzy.fodt tex/2_analiza_wiedzy
+start "" 9>"%lock%3" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 3_teoria_problemu.fodt tex/3_teoria_problemu
+start "" 9>"%lock%4" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 4_praktyka_implementacja.fodt tex/4_praktyka_implementacja
+start "" 9>"%lock%5" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 5_rezultaty_analiza.fodt tex/5_rezultaty_analiza
+start "" 9>"%lock%6" /b /realtime java -jar %W2L_PATH%\writer2latex.jar -config mycfg.xml 6_zakonczenie_podsumowanie.fodt tex/6_zakonczenie_podsumowanie
 
 
 :Wait for all processes to finish (wait until lock files are no longer locked)

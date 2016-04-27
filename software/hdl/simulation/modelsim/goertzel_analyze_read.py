6b05f18cb67792a7dcb86f16e7d9c8058cb35a8e
kkthx <lukasz@kkthx.pl>
2016-04-27 14:23:11 +0200

    sw: simulation - python signal analyze script

--- software/hdl/simulation/modelsim/goertzel_analyze_read.py
+++ software/hdl/simulation/modelsim/goertzel_analyze_read.py
@@ -7,16 +7,10 @@ import scipy.signal
 
 
 
-
-
-
-
-
-
 vals=[]
 with open('goertzel_analyze_fast.txt','r') as f:
 	for val in f:
-		if (val[0] != '/' and val[0] in '0123456789abcdef'):
+		if (val[0] in '0123456789abcdef'):
 			vals.append(int(val,16))
 
 test=[]
@@ -28,8 +22,6 @@ for step in range(0, len(vals)-250, 250):
 	zeros=0
 	ones=0
 	for step2 in range(249):
-		if (step > 179000):
-			print(len(vals),step, step2, step+step2, vals[step+step2])
 		if (vals[step+step2] > 500):
 			ones += 1
 		else:
@@ -48,6 +40,37 @@ mpl.grid()
 mpl.plot(test,'o-')
 
 
+start_of_minute = False
+decoded_bits = []
+ones=0
+zeros=0
+for num, val in enumerate(test):
+
+	if (start_of_minute):
+		decoded_bits = []
+
+	if (val == 1):
+		ones += 1
+		if (zeros == 1):
+			decoded_bits.append(0)
+		if (zeros == 2):
+			decoded_bits.append(1)
+		zeros = 0
+
+
+	if (val == 0):
+		start_of_minute = False
+		ones = 0
+		zeros += 1
+
+
+	if ((ones > 10) and not start_of_minute): # detect start of minute indicator
+		zeros = 0
+		start_of_minute = True
+		print('\n')
+		print(decoded_bits)
+		print("start_of_minute")
+		mpl.axvline(num, color='red')
 
 
 
